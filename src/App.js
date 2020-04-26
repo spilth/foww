f015d46a29f08226a83ff4eea7a605cee9f8ce1f
Brian Kelly <polymonic@gmail.com>
2020-04-26 17:19:22 -0500

    Adds Bootstrap as a dependency and preps a nav for future features

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React from "react";
 import { UnitTable } from "./UnitTable";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
+import NavItem from "react-bootstrap/NavItem";
+import NavLink from "react-bootstrap/NavLink";
 
-function App() {
+const App = () => {
   return (
     <div className="container">
-      <h1>Fallout Wasteland Warfare</h1>
+      <h1>Fallout: Wasteland Warfare</h1>
+      <Router>
+        <div>
+          <Nav variant="tabs" className="mt-2 mb-2">
+            <LinkContainer to="/">
+              <NavItem>
+                <NavLink href="/" active={true}>
+                  Unit List
+                </NavLink>
+              </NavItem>
+            </LinkContainer>
+          </Nav>
 
-      <h2>Units</h2>
+          <Switch>
+            <Route path="/">
+              <UnitTable />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
 
-      <UnitTable/>
-
-      <h6>Source Code: <a href="https://github.com/spilth/foww">https://github.com/spilth/foww</a></h6>
+      <h6 className="text-center">
+        Source Code:{" "}
+        <a href="https://github.com/spilth/foww">
+          https://github.com/spilth/foww
+        </a>
+      </h6>
     </div>
   );
-}
+};
 
 export default App;
